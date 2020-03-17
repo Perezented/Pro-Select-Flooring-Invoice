06c591641f6fca89b3508fe586fdaa02c0f66ee6
Perezented <57721734+Perezented@users.noreply.github.com>
2020-03-16 20:48:19 -0500

    Added the less file. I started working the page layout how I am wanting it to look. Put in some filler information for the time being until I get js variables going.

--- invoice/src/Invoice.js
+++ invoice/src/Invoice.js
@@ -1,34 +1,45 @@
 import React from "react";
-
+import "../src/css/invoice.css";
 function Invoice() {
     const day = new Date();
 
     const today = `${day.getMonth()}/${day.getDate()}/${day.getFullYear()}`;
 
     return (
-        <div className="App">
+        <div className="invoice">
             <header>
                 <h1>Pro-Select Flooring LLC</h1>
-                <h2>INVOICE</h2>
+                <h1 className="grey">INVOICE</h1>
             </header>
-            <section>
+            <section className="topSection">
                 <div className="address">
+                    <p>Ricardo Castaneda</p>
                     <address>PO Box 612</address>
                     <address>Bixby, OK 74008</address>
                     <address>(918)804-1026</address>
                 </div>
                 <div className="numbersDate">
-                    <p>Date: {today}</p>
-                    <p>Invoice: </p>
-                    <p>Work Order: </p>
+                    <h5>Date:</h5>
+                    <p>{today}</p>
+                    <h5>Invoice:</h5>
+                    <p>Invoice </p>
+                    <h5>Work Order:</h5>
+                    <p>Work Order</p>
                 </div>
             </section>
             <section className="billTo">
                 <h3>Bill To:</h3>
-                <p>Name:</p>
-                <p>Company:</p>
-                <p>Email:</p>
-                <p>Phone Number:</p>
+                <h3> </h3>
+                <div>
+                    <h5>Name:</h5>
+                    <p>Name</p>
+                    <h5>Company:</h5>
+                    <p>Comapany</p>
+                    <h5>Email:</h5>
+                    <p>Email</p>
+                    <h5>Phone Number:</h5>
+                    <p>Phone Number</p>
+                </div>
             </section>
             <section className="table">
                 <div className="mainTableOutline">
@@ -37,15 +48,16 @@ function Invoice() {
                         <h4>Amount</h4>
                         <h4>Total</h4>
                     </div>
-                    <div className="bottomTotal">
-                        <h4>Total</h4>
-                    </div>
                 </div>
             </section>
+            <div className="bottomTotal">
+                <h4>Total</h4>
+                <p>Number Total</p>
+            </div>
             <section className="thanks">
                 <p>
                     Due upon receipt, please make all checks payable to
-                    <span className="bold">Pro-Select Flooring</span>
+                    <span className="bold"> Pro-Select Flooring</span>
                 </p>
                 <p>
                     If you have any questions concerning this invoice, text or
